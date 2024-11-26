fb0ebc31c646fef8d10035e281d60ed503476297
fredtchiadeu <fred.tchiadeu@pategou-consulting.com>
2024-11-26 17:57:08 +0100

    test: fetch todo from page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
 import Image from "next/image";
+import {TodoService} from "@/src/service/TodoService";
+import {Todo} from "@/src/dto/todo.dtos";
+import {TodoError} from "@/src/exception/todo.exceptions";
 
-export default function Home() {
+export default async function Home() {
+  const todos:Todo[] | TodoError= await TodoService.getAllTodos();
+  // if(todos instanceof TodoError){}
+  console.log("Todos loaded", todos);
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
