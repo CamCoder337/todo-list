8d1c459ffdb1a5be6dd441ce7736fdde3010d673
fredtchiadeu <fred.tchiadeu@pategou-consulting.com>
2024-11-27 10:37:54 +0100

    add: TodoStatus type

--- src/dto/todo.dtos.ts
+++ src/dto/todo.dtos.ts
@@ -1,12 +1,12 @@
+export type TodoStatus = 'TODO' | 'IN_PROGRESS' | 'DONE';
 export interface TodoBase {
     title: string;
     description: string;
     priority: number;
-    status: 'TODO' | 'IN_PROGRESS' | 'DONE';
 }
 
 export interface TodoCreate extends TodoBase {
-    status: 'TODO' | 'IN_PROGRESS' | 'DONE'
+    status: TodoStatus
 }
 
 export interface Todo extends TodoCreate {
