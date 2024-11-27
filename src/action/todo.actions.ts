6a770e0360d6f60f833330a1365cd8b1524ee35c
fredtchiadeu <fred.tchiadeu@pategou-consulting.com>
2024-11-27 14:40:00 +0100

    cleaning ⚡

--- src/action/todo.actions.ts
+++ src/action/todo.actions.ts
@@ -4,49 +4,45 @@ import axios from "axios";
 import {Todo, TodoCreate} from "@/src/dto/todo.dtos";
 
 export async function saveATodo(endpoint: string, todo: TodoCreate): Promise<Todo> {
-    const response =  await axios
+    return await axios
         .post(endpoint, todo)
-        .then((result)=> {
+        .then((result) => {
             return result.data as Todo;
-        }).catch((error)=>{
+        }).catch((error) => {
             console.error(error);
             throw error;
         });
-    return response;
 }
 
 export async function findAllTodos(endpoint: string): Promise<Todo[]> {
-    const response =  await axios
+    return await axios
         .get(endpoint)
-        .then((result)=> {
+        .then((result) => {
             return result.data as Todo[];
-        }).catch((error)=>{
+        }).catch((error) => {
             console.error(error);
             throw error;
         });
-    return response;
 }
 
 export async function findATodoById(endpoint: string,todoId:string): Promise<Todo> {
-    const response =  await axios
-        .get(endpoint+`/${todoId}`)
-        .then((result)=> {
+    return await axios
+        .get(endpoint + `/${todoId}`)
+        .then((result) => {
             return result.data as Todo;
-        }).catch((error)=>{
+        }).catch((error) => {
             console.error(error);
             throw error;
         });
-    return response;
 }
 
 export async function deleteATodoById(endpoint: string,todoId:string): Promise<Todo> {
-    const response =  await axios
-        .delete(endpoint+`/${todoId}`)
-        .then((result)=> {
+    return await axios
+        .delete(endpoint + `/${todoId}`)
+        .then((result) => {
             return result.data as Todo;
-        }).catch((error)=>{
+        }).catch((error) => {
             console.error(error);
             throw error;
         });
-    return response;
 }
