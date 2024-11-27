38b3b5ae49c97f1ccc627f71043bcce2390b921f
fredtchiadeu <fred.tchiadeu@pategou-consulting.com>
2024-11-27 14:31:22 +0100

    add: todo status color

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,19 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import {TodoStatus} from "@/src/dto/todo.dtos";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function todoStatusColor(status: TodoStatus): "to_do" | "progress" | "done" {
+  switch (status) {
+    case "TODO":
+      return "to_do";
+    case "IN_PROGRESS":
+      return "progress";
+    case "DONE":
+      return "done";
+  }
+}
+
